f7e38eb2307c0ca47f1f74c3cc18515ba7667441
Priyanshu <Rose@IdeaPad.localdomain>
2022-10-16 12:56:01 +0530

    Frist Update!

--- js/menikah.js
+++ js/menikah.js
@@ -1,26 +1,26 @@
 // Get that hamburger menu cookin' //
 
-document.addEventListener("DOMContentLoaded", function() {
-  // Get all "navbar-burger" elements
-  var $navbarBurgers = Array.prototype.slice.call(
-    document.querySelectorAll(".navbar-burger"),
-    0
-  );
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-    // Add a click event on each of them
-    $navbarBurgers.forEach(function($el) {
-      $el.addEventListener("click", function() {
-        // Get the target from the "data-target" attribute
-        var target = $el.dataset.target;
-        var $target = document.getElementById(target);
-        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle("is-active");
-        $target.classList.toggle("is-active");
-      });
-    });
-  }
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//   // Get all "navbar-burger" elements
+//   var $navbarBurgers = Array.prototype.slice.call(
+//     document.querySelectorAll(".navbar-burger"),
+//     0
+//   );
+//   // Check if there are any navbar burgers
+//   if ($navbarBurgers.length > 0) {
+//     // Add a click event on each of them
+//     $navbarBurgers.forEach(function($el) {
+//       $el.addEventListener("click", function() {
+//         // Get the target from the "data-target" attribute
+//         var target = $el.dataset.target;
+//         var $target = document.getElementById(target);
+//         // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+//         $el.classList.toggle("is-active");
+//         $target.classList.toggle("is-active");
+//       });
+//     });
+//   }
+// });
 
 // Smooth Anchor Scrolling
 $(document).on("click", 'a[href^="#"]', function(event) {
@@ -39,7 +39,7 @@ window.onscroll = function() {
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
     document.getElementById("toTop").style.display = "block";
   } else {
     document.getElementById("toTop").style.display = "none";
